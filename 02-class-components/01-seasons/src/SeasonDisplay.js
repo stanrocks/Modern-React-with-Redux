03ee67133f07f6594a97264634e8c3b9ddb602d3
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-09-02 11:33:28 +0500

    extract options to config objects

--- 02-class-components/01-seasons/src/SeasonDisplay.js
+++ 02-class-components/01-seasons/src/SeasonDisplay.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+const seasonConfig = {
+  summer: { text: "Let's hit the beach!", iconName: 'sun' },
+  winter: { text: 'Burr, it is chilly!', iconName: 'snowflake' },
+};
+
 const getSeason = (lat, month) => {
   // months from 0 to 2 and from 9 to 11 defines winter for northern hemisphere (latitude > 0) and summer for southern hemisphere
   if (month > 2 && month < 9) {
@@ -12,16 +17,19 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
   // example of ternary expression, result is put to text var
-  const text =
-    season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+  // const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
   // get name of font-awesome icon that correspond with season
-  const icon = season === 'winter' ? 'snowflake' : 'sun';
+  // const icon = season === 'winter' ? 'snowflake' : 'sun';
+
+  // refactor using seasonConfig:
+  const { text, iconName } = seasonConfig[season]; // { text, iconName }
+
   // console.log(season);
   return (
     <div>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
       <h1>{text}</h1>
-      <i className={`${icon} icon`} />
+      <i className={`${iconName} icon`} />
     </div>
   );
 };
