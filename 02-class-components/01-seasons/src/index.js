dd1567187b64e82cba6c1027066f57ad9085dd06
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-09-02 07:40:45 +0500

    refactor to class-based component

--- 02-class-components/01-seasons/src/index.js
+++ 02-class-components/01-seasons/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-  );
+// ====== function-based component ======
+// const App = () => {
 
-  return <div>Latitude: </div>;
-};
+//   window.navigator.geolocation.getCurrentPosition(
+//     (position) => console.log(position),
+//     (err) => console.log(err)
+//   );
 
+//   return <div>Latitude: </div>;
+// };
+
+// ====== class-based component ======
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude:</div>;
+  }
+}
 ReactDOM.render(<App />, document.querySelector('#root'));
