f87c21642ae147f51fcbdae557a4b26b5e0d42a4
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-09-02 13:11:20 +0500

    specifying default props

--- 02-class-components/01-seasons/src/index.js
+++ 02-class-components/01-seasons/src/index.js
@@ -42,7 +42,7 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <Spinner />;
+    return <Spinner message="Please accept location request" />;
   }
 }
 ReactDOM.render(<App />, document.querySelector('#root'));
