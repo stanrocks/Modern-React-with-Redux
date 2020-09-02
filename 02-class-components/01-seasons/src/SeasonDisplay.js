ae12114c2c2c1cedbf3063984369b745e7b80f73
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-09-02 11:11:47 +0500

    determing season

--- 02-class-components/01-seasons/src/SeasonDisplay.js
+++ 02-class-components/01-seasons/src/SeasonDisplay.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
+const getSeason = (lat, month) => {
+  // months from 0 to 2 and from 9 to 11 defines winter for northern hemisphere (latitude > 0) and summer for southern hemisphere
+  if (month > 2 && month < 9) {
+    return lat > 0 ? 'summer' : 'winter';
+  } else {
+    return lat > 0 ? 'winter' : 'summer';
+  }
+};
+
 const SeasonDisplay = (props) => {
-  console.log(props.lat);
+  const season = getSeason(props.lat, new Date().getMonth());
+  console.log(season);
   return <div>Season Display</div>;
 };
 
