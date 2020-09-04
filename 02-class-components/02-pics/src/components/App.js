589fe219d733120b75f597439e23480da4262c96
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-09-05 04:13:05 +0500

    request params

--- 02-class-components/02-pics/src/components/App.js
+++ 02-class-components/02-pics/src/components/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
   // that function is passing down to a child (search bar) as a prop
   onSearchSubmit(term) {
-    console.log(term);
+    // console.log(term);
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term,
+      },
+      headers: {
+        Authorization: 'Client-ID XuNzyZOVv6xRMZ2bCmpqEi5J7PsUJhXpvn8sSH305lE',
+      },
+    });
   }
   render() {
     return (
