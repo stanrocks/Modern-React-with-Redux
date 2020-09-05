45bf3fe41324c60065676f5581eed0a43dbcd0e6
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-09-05 05:21:16 +0500

    get result from response and bind callback to proper context using arrow f

--- 02-class-components/02-pics/src/components/App.js
+++ 02-class-components/02-pics/src/components/App.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
+  state = { images: [] };
+
   // that function is passing down to a child (search bar) as a prop
-  async onSearchSubmit(term) {
+  onSearchSubmit = async (term) => {
     // console.log(term);
     const response = await axios.get('https://api.unsplash.com/search/photos', {
       params: {
@@ -15,13 +17,15 @@ class App extends React.Component {
       },
     });
 
-    console.log(response.data.results);
-  }
+    // console.log(response.data.results);
+    this.setState({ images: response.data.results });
+  };
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
