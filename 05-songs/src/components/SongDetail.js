007bbc029c494cd0bccef183751bae4b14cc7cd3
Stanley Rocks <25880632+stanrocks@users.noreply.github.com>
2020-09-07 10:01:41 +0500

    conditional rendering

--- 05-songs/src/components/SongDetail.js
+++ 05-songs/src/components/SongDetail.js
@@ -3,11 +3,25 @@ import { connect } from 'react-redux';
 
 const SongDetail = (props) => {
   console.log('Song Detail props:', props);
-  return <div>Song Detail</div>;
+  const { mySelectedSong } = props;
+  // if song is not selected yet
+  if (!mySelectedSong) {
+    return <div>Select a song</div>;
+  }
+  return (
+    <div>
+      <h3>Details for:</h3>
+      <p>
+        Title: {mySelectedSong.title}
+        <br />
+        Duration: {mySelectedSong.duration}
+      </p>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
-  return { song: state.selectedSong };
+  return { mySelectedSong: state.selectedSong };
 };
 
 export default connect(mapStateToProps)(SongDetail);
